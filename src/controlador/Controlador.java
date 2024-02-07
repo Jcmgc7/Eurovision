cddc18c4846965d5dc7143e3e25110f40685d527
alvar <alvaro2004gb13@gmail.com>
2024-02-07 13:40:00 +0100

    4º commit

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -3,15 +3,27 @@ package controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import vista.Vista;
 
 public class Controlador implements ActionListener{
 	Vista vista = new Vista();
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		SessionFactory SF = null;
+		try {
+			Configuration configuration = new Configuration();
+			configuration.configure("hibernate.cfg.xml");
+			SF = configuration.buildSessionFactory();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
 	}
 	public Controlador(Vista frame) {
+		
+	}
+	public void consultaPais() {
 		
 	}
 }
